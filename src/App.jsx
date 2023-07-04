7e07d024b3d980a32cf62a0be93c61dd3e09c6f5
rcalle <rcalle@adrformacion.com>
2023-07-04 14:33:22 +0200

    casi acabado MainMenu

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,11 @@ import MainMenu from './components/screens/MainMenu';
     imageSource: 'https://d2mxgq73d0f24i.cloudfront.net/images/imagenes/img_6893755_2184.jpg'
   }
 
+  const mockGame = {
+    type: 'Relacionar movidas',
+    title: 'Componentes de React',
+    imageSource: 'https://es.rcalle.dev.educaplay.com/v2/r1/img/activities/RELACIONAR_COLUMNAS/logoActivity.png?v=20230202'
+  }
 
 function App() {
 
@@ -14,7 +19,7 @@ function App() {
   }
 
   return (
-    <MainMenu user={mockUser} onStart={handleClick}/>
+    <MainMenu user={mockUser} onStart={handleClick} gameData={mockGame} />
   )
 }
 
--- src/components/screens/MainMenu.jsx
+++ src/components/screens/MainMenu.jsx
@@ -3,21 +3,34 @@ import RestartIcon from '../../icons/RestartIcon'
 import StartIcon from '../../icons/StartIcon';
 import Button from '../Button/Button'
 import PropTypes from 'prop-types';
+import EducaplayLogo from '../EducaplayLogo';
+import classes from './mainMenu.module.scss'
 
 function MainMenu(props) {
-    const { onStart, user } = props;
+    const { onStart, user, gameData } = props;
 
     return (
         <>
-            <h1>Yes or No</h1>
-            <ActivityLogin user={user} />
-            <Button className='classssse' icon={<StartIcon />} onClick={onStart}>
-                Comenzar
-            </Button>
-            <hr />
-            <Button className='classssse' icon={<RestartIcon />}>
-                Reintentar
-            </Button>
+            <div className={classes.background}>
+                <div className={classes.logoHeader}><EducaplayLogo width={96} height={88} /></div>
+                <div className={classes.gameData}>
+                    <div className={classes.imageWrapper}>
+                        <img className={classes.image} width="360" height="360" src={gameData.imageSource} alt={gameData.type} />
+                    </div>
+                    <div className={classes.titleWrapper}>
+                        <h1 className={classes.gameTitle}>{gameData.title}</h1>
+                        <h2 className={classes.gameType}>{gameData.type}</h2>
+                    </div>
+                </div>
+                <ActivityLogin user={user} />
+                <Button className='classssse' icon={<StartIcon />} onClick={onStart}>
+                    Comenzar
+                </Button>
+                {/* <hr />
+                <Button className='classssse' icon={<RestartIcon />}>
+                    Reintentar
+                </Button> */}
+            </div>
         </>
     );
 }
@@ -27,6 +40,11 @@ MainMenu.propTypes = {
     user: PropTypes.shape({
         name: PropTypes.string,
         lastname: PropTypes.string,
+        imageSource: PropTypes.string
+    }),
+    gameData: PropTypes.shape({
+        type: PropTypes.string,
+        title: PropTypes.string,
         imageSource: PropTypes.string
     })
 }
