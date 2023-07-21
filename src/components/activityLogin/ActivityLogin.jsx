2028ba61a3a46f5eb5d3ff93654579d263f4bcc1
rcalle <rcalle@adrformacion.com>
2023-07-21 10:59:35 +0200

    estilos activityLogin

--- src/components/activityLogin/ActivityLogin.jsx
+++ src/components/activityLogin/ActivityLogin.jsx
@@ -1,22 +1,35 @@
 import PropTypes from 'prop-types';
 import classes from './activityLogin.module.scss'
+import defaultAvatar from '../../assets/usuario.svg'
 
 function ActivityLogin(props) {
     const user = props.user;
-    const logged = true; //is the user logged
+    const logged = false; //is the user logged
 
-    return (logged &&
-        <div className={classes.activityLogin}>
-            <span className={classes.activityLogin__user}>
-                <img src={user.imageSource} width="74" height="76" alt=""/>
-            </span>
-            <div className={classes.activityLogin__txt}>
-                <span className={classes.activityLogin__identified}></span>
-                <span className={classes.title}>
-                    { user.name } {user.lastname}
+    if (logged) {
+        return (
+            <div className={classes.activityLogin}>
+                <span className={classes.userAvatar}>
+                    <img src={user.imageSource} width="74" height="76" alt=""/>
                 </span>
-                <span className={classes.activityLogin__identified2}></span>
+                <div className={classes.txt}>
+                    <span className={classes.identified}></span>
+                    <span className={classes.title}>
+                        { user.name } {user.lastname}
+                    </span>
+                </div>
             </div>
+        )
+    }
+
+    return (
+        <div className={classes.activityLogin}>
+                <span className={classes.userAvatar}>
+                    <img src={defaultAvatar} width="74" height="76" alt=""/>
+                </span>
+                <div className={classes.txt}>
+                    <a className={classes.notIdentified} href="//es.educaplay.com/">Pulsa aquí para identificarte</a>
+                </div>
         </div>
     );
 }
