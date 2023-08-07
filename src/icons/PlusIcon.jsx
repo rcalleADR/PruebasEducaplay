fa1f283226c9f97d92ca801a5234f6960c53eeca
rcalle <rcalle@adrformacion.com>
2023-08-07 13:46:24 +0200

    maquetacion y boton resultados

---
+++ src/icons/PlusIcon.jsx
@@ -0,0 +1,10 @@
+function PlusIcon() {
+    return (
+        <svg width="16" height="16" viewBox="0 0 16 16" fill="none">
+            <path d="M8 1V15" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path>
+            <path d="M1 8H15" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path>
+        </svg>
+    );
+}
+
+export default PlusIcon;
